7f7703177df210d9fe0591bae96bf1b1aa6ffb5e
Shihta Kuan <kuster.die9841@gmail.com>
2015-08-24 15:04:40 +0800

    Added frontend and config

---
+++ frontend/static/nanny.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+    console.log('JS loaded!');
+    jQuery('center').remove();
+    jQuery('#links-row-1').remove();
+    jQuery('#content').css('width', '77%');
+    jQuery('#links').css('width', '20%');
+    jQuery('#links-row-2').css('width', '100%');
+});
